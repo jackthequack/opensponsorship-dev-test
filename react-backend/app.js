80c4fa4bc5a1fc8a4c46f2c727207eeab349e63b
Jack Langberg <jacklangberg2121@gmail.com>
2021-03-22 11:08:36 -0400

    Configuring for Heroku

--- react-backend/app.js
+++ react-backend/app.js
@@ -18,7 +18,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.set('port', process.env.PORT || 3000);
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -59,5 +59,10 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/../profile-builder/src', 'index.html'));
+});
+
 
 module.exports = app;
