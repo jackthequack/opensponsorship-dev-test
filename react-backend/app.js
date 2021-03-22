0354875d6c2521f35c22b28ce2a410ba004f199b
Jack Langberg <jacklangberg2121@gmail.com>
2021-03-22 13:08:28 -0400

    Configuring for Heroku

--- react-backend/app.js
+++ react-backend/app.js
@@ -13,7 +13,31 @@ var usersRouter = require('./routes/users');
 var createRouter = require('./routes/create');
 var updateRouter = require('./routes/update')
 const serveReact = require('./routes/react')
+const mongoose = require('mongoose');
+const mongoDB = 'mongodb://localhost:27017/profiles'
+
+const User = require('./user_model.js')
+
+
 require('dotenv').config()
+
+mongoose.connect(process.env.MONGODB_URI || mongoDB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+      cb(null, './public/images');
+  },
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
+let upload = multer({storage: storage});
+
 var app = express();
 const port = process.env.PORT || 5000;
 // view engine setup
@@ -40,30 +64,111 @@ const storage = multer.diskStorage({
 
 
 let upload = multer(storage);
+app.get('/users', (req, res) => {
+  User.find({}, (err, userList) => {
+    res.send(userList)
+  })
+})
+app.post('/create', (req, res) => {
+  let newId;
+  const newProfile = {};
+  newProfile.firstName = req.body.firstName,
+  newProfile.lastName = req.body.lastName,
+  newProfile.sports = req.body.sports,
+  newProfile.gender = req.body.gender,
+  newProfile.dateOfBirth = req.body.dateOfBirth,
+  newProfile.description = req.body.description,
+  newProfile.team = req.body.team,
+  newProfile.location = req.body.location
+  newProfile.profilePic = '/images/' + req.file.filename
+      
+      console.log("Profile: " + req.file)
+     console.log(req.file.filename)
 
-// app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/create', createRouter);
-app.use('/update', updateRouter)
-app.use(function(req, res, next) {
+      let userInstance = new User(newProfile);
+      console.log(userInstance)
+      userInstance.save((err) => {
+          if(err){
+              console.log("ERROR")
+              return err;
+          }
+          else{
+              console.log("saved");
+              res.status(200).end();
+          }
+          
+      })
+      
+})
+app.put('/update', (req, res) => {
+    console.log("File: " + req.file)
+    console.log("Profile: " + req.body.profilePic)
+    const newProfile = {};
+
+    newProfile.firstName = req.body.firstName,
+    newProfile.lastName = req.body.lastName,
+    newProfile.sports = req.body.sports,
+    newProfile.gender = req.body.gender,
+    newProfile.dateOfBirth = req.body.dateOfBirth,
+    newProfile.description = req.body.description,
+    newProfile.team = req.body.team,
+    newProfile.location = req.body.location
+    if(req.file) {
+        newProfile.profilePic = '/images/' + req.file.filename;
+    }
+    else {
+        newProfile.profilePic = req.body.profilePic
+    }
+        console.log(newProfile.profilePic)
+    console.log(__dirname)
+    User.findById(req.body["_id"], (err, oldUser) => {
+        if(newProfile.profilePic != oldUser.profilePic){
+            fs.unlinkSync(path.join(__dirname, '..', 'public', oldUser.profilePic), (err) => {
+                if(err) throw err;
+                console.log("Deleted:", oldUser.profilePic)
+            })
+            User.findByIdAndUpdate(req.body["_id"], newProfile, {new: true}, (err, newUser) => {
+                console.log("Updated user: ", newUser)
+                res.status(200).send(newUser);
+            });
+        }
+        else{
+            User.findByIdAndUpdate(req.body["_id"], newProfile, (err, newUser) => {
+                console.log("Updated user: ", newUser)
+                res.status(200).end();
+            });
+        }
+        
+    })
+   
+})
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/../profile-builder/src/index.js'));
 
 })
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+// app.use('/create', createRouter);
+// app.use('/update', updateRouter)
+// app.use(function(req, res, next) {
+//   res.sendFile(path.join(__dirname + '/../profile-builder/src/index.js'));
+
+// })
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
 
 
