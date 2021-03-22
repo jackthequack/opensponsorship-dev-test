c463f9fade8b0f7219f36d4df10000085c126ff8
Jack Langberg <jacklangberg2121@gmail.com>
2021-03-22 12:52:13 -0400

    Configuring for Heroku

--- react-backend/routes/react.js
+++ react-backend/routes/react.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('*', (req, res, next) = () => {
+router.get((req, res, next) = () => {
     res.sendFile(path.join(__dirname + '/.../profile-builder/src/index.js'));
      
 })
